2649c59dcdb6941b45ee9b9466530a53e2035bbe
FateRiddle <fateriddle@gmail.com>
2020-06-28 10:23:44 +0800

    Updates

---
+++ precache-manifest.850928c8153fffa0057f325ab56b3c03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f832027f5b9791c2e0c4d8f8d3aea02",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d51c45c485bba8ef5d42",
+    "url": "./static/css/2.f6156d2c.chunk.css"
+  },
+  {
+    "revision": "d7ad8a1636c93235f208",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d51c45c485bba8ef5d42",
+    "url": "./static/js/2.98bb404d.chunk.js"
+  },
+  {
+    "revision": "8320cd00332450aeb37919ea2c5bcf02",
+    "url": "./static/js/2.98bb404d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ad8a1636c93235f208",
+    "url": "./static/js/main.440dd132.chunk.js"
+  },
+  {
+    "revision": "d7cedc736bd09d56474f",
+    "url": "./static/js/runtime-main.1c11c82f.js"
+  }
+]);
