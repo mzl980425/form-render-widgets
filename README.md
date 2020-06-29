1d899889e4f6a794346e360e4d58f07c5c46db81
FateRiddle <fateriddle@gmail.com>
2020-06-29 10:25:53 +0800

    换一个editor

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # form-render-widgets
 
+[![NPM](https://img.shields.io/npm/v/form-render-widgets.svg)](https://www.npmjs.com/package/form-render-widgets) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
 ## Install
 
 ```bash
--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
-import React, { useRef } from 'react'
-import JoditEditor from 'jodit-react'
+import React, { useState } from 'react'
+import BraftEditor from 'braft-editor'
+import 'braft-editor/dist/index.css'
 
-const RichTextEditor = ({ onChange, value, name, options, ...rest }) => {
-  const editor = useRef(null)
+const { createEditorState } = BraftEditor
 
-  const handleSubmit = (value) => {
-    onChange(name, value)
+const EditorDemo = ({ name, onChange, value, ...rest }) => {
+  const [editor, set] = useState(() => createEditorState(value))
+
+  const handleChange = (editor) => {
+    set(editor)
+    const htmlContent = editor.toHTML()
+    onChange(name, htmlContent)
   }
 
-  const handleChange = (value) => {}
+  const onSave = () => {
+    // Pressing ctrl + s when the editor has focus will execute this method
+    // Before the editor content is submitted to the server, you can directly call editorState.toHTML () to get the HTML content
+    // const htmlContent = editor.toHTML()
+    // console.log(htmlContent)
+    // onChange(name, htmlContent)
+  }
 
   return (
-    <div style={{ width: '100%' }}>
-      <JoditEditor
-        ref={editor}
-        value={value}
-        config={options}
-        tabIndex={1} // tabIndex of textarea
-        onBlur={handleSubmit}
+    <div style={{ border: '1px solid rgba(0,0,0,0.2)' }}>
+      <BraftEditor
+        contentStyle={{ minHeight: 100, height: 'auto', maxHeight: 500 }}
+        value={editor}
         onChange={handleChange}
-        {...rest}
+        onSave={onSave}
+        onBlur={onSave}
       />
     </div>
   )
 }
 
-export default RichTextEditor
+export default EditorDemo
